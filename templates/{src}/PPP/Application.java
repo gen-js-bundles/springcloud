416d1d294b9c5e6007ba02ba9289721667e114b9
Ludovic Chaboud <l.chaboud@gmail.com>
2015-03-12 15:59:42 +0100

    Update Application.java

--- templates/{src}/PPP/Application.java
+++ templates/{src}/PPP/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+<% if(has(all.config.tags.build.dependencies, 'artifactId', 'spring-cloud-config-server')) { %>
+@EnableConfigServer
+<% } %>
 public class Application {
 
     public static void main(String[] args) {
